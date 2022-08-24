4821055834d59155db584b231549a910b34dce1a
714940 <714940@cognizant.com>
2022-08-24 10:29:10 -0300

    First version

---
+++ ConsoleWithLocalization/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+using Domain.Resources;
+
+namespace ConsoleWithLocalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Default = System Default
+            Console.WriteLine(Messages.WELCOME);
+
+            ChangeCulture("de");
+            Console.WriteLine(Messages.WELCOME);
+
+            ChangeCulture("es");
+            Console.WriteLine(Messages.WELCOME);
+
+            ChangeCulture("en");
+            Console.WriteLine(Messages.WELCOME);
+
+            ChangeCulture("it"); //Doesn't exist
+            Console.WriteLine(Messages.WELCOME);
+        }
+
+        static void ChangeCulture(string name)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(name);
+        }
+    }
+}
